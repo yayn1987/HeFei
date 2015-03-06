6c05dc7a89bb5f64623124d70965c334074e84d7
小秋 <wangyg_89@163.com>
2015-03-06 20:24:18 +0800

    建立项目
    项目建立

---
+++ HeFei.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeFei.Core
+{
+    public class Class1
+    {
+    }
+}
